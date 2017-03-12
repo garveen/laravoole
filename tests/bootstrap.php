91662f7ff48b4e6e449143a5802e3954077f1ce4
Gavin <acabin@live.com>
2017-03-13 04:04:04 +0800

    test for laravel 5.2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,8 @@ use stdClass;
 require __DIR__ . '/../vendor/autoload.php';
 
 if (!class_exists(Broadcaster::class)) {
-    class_alias(stdClass::class, Broadcaster::class);
+	class Foo {}
+    class_alias(Foo::class, Broadcaster::class);
 }
 
 function env($key, $default = null)
